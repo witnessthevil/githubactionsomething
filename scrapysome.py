95477409dc8db722fe36651f141e69e874491cde
danie <yytamab@connect.ust.hk>
2022-11-22 13:33:32 +0800

    some

---
+++ scrapysome.py
@@ -0,0 +1,21 @@
+import scrapy
+
+from scrapy.crawler import CrawlerProcess
+
+class first_spider(scrapy.Spider):
+    name = "first spider"
+    start_urls = ["https://scrapy.org/"]
+
+    def parse(self, response, **kwargs):
+        
+        json = {
+            "target" : response.xpath('//div[@class="block-left"]/p[2]/text()').get()
+        }
+
+        yield json
+
+
+
+process = CrawlerProcess()
+process.crawl(first_spider)
+process.start()
